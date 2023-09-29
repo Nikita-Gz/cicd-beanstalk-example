845dc42fe52764009f27a58c0ca242d19b93a51f
Nikita-Gz <ngez.work@gmail.com>
2023-09-29 18:51:27 +0300

    fixed several PyLint complains

--- exampleproject/exampleapp/tests.py
+++ exampleproject/exampleapp/tests.py
@@ -2,11 +2,11 @@ from django.test import TestCase
 
 
 class ExampleTest(TestCase):
-  def setUp(self):
-    pass
+    def setUp(self):
+        pass
 
-  def test1(self):
-    self.assertEqual(1, 2 - 1)
+    def test1(self):
+        self.assertEqual(1, 2 - 1)
 
-  def test2(self):
-    self.assertEqual(1 + 2, 2 + 1)
+    def test2(self):
+        self.assertEqual(1 + 2, 2 + 1)
--- exampleproject/exampleapp/views.py
+++ exampleproject/exampleapp/views.py
@@ -1,6 +1,4 @@
-from django.shortcuts import render
-
 from django.http import HttpResponse
 
 def index(request):
-  return HttpResponse("Hi! This is the main view of my CI/CD example app!")
+    return HttpResponse("Hi! This is the main view of my CI/CD example app!")
