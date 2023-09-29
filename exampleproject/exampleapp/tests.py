a2bfa29a6cbf85c71c4a2f6e4a0d1628716187d9
Nikita-Gz <ngez.work@gmail.com>
2023-09-29 18:32:58 +0300

    added example tests

--- exampleproject/exampleapp/tests.py
+++ exampleproject/exampleapp/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
 
-# Create your tests here.
+
+class ExampleTest(TestCase):
+  def setUp(self):
+    pass
+
+  def test1(self):
+    self.assertEqual(1, 2 - 1)
+
+  def test2(self):
+    self.assertEqual(1 + 2, 2 + 1)
